1a3455c252c37c2024b98c44e4f687768de7a265
Theo Bouwman <theobouwman98@gmail.com>
2020-11-04 16:09:50 +0100

    Update login.py

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/login.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/login.py
@@ -36,7 +36,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
-            user.id, expires_delta=access_token_expires
+            user.id, expires_delta=access_token_expires, scopes=form_data.scopes
         ),
         "token_type": "bearer",
     }
